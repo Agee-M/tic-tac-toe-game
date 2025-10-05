64bf35b3dea179de5ca9ef90e75836ebf6809246
agee <149341901+Agee-M@users.noreply.github.com>
2025-10-05 13:14:52 -0700

    Add player turns and refactor player keys
    Added a way to keep track of turns along with minor refactors:
    - Determine who plays first
    - Consolidated additional player data into players dict
    - Made players dict easier to read and parse

--- game.py
+++ game.py
@@ -2,9 +2,20 @@
 
 def game():
     # Initialize game
-    # game_board, selection_board, players, is_select = start()
-    game_board, selection_board = new_boards()
-    # is_select = False
+    game_board, selection_board, players, is_select = start()
+
+    # Interactivity
+    if players["Player1"]["Turn"] == True:
+        input(f"{players["Player1"]["Name"]} select a spot: ")
+        players["Player1"]["Turn"] = False
+        players["Player1"]["Turn"] = True
+
+    else:
+        input(f"{players["Player2"]["Name"]} select a spot: ")
+        players["Player1"]["Turn"] = True
+        players["Player1"]["Turn"] = False
+
+
     game_board[2][2] = 'X'
     game_board[0][2] = 'O'
     game_board[1][1] = 'X'
@@ -16,15 +27,13 @@ def game():
     print_board(game_board, selection_board, is_select = True)
     #print_players(players)
 
-
-    
-
     # Show positions as matrix (1 - 9) 
     # Combine game board with choice board (or just remove dots for choice board)
     # TODO: Add interactivity between choices and board
     # TODO: Add win conditions (including tie)
     # TODO: Add reset functionality
     # TODO: Add winner goes first?
+    # TODO: Make 1 player or 2 player
 
 
     return
@@ -36,47 +45,75 @@ def start():
     # Initialize new boards
     board1, board2 = new_boards()
 
+    # Set selection state
+    state = False
+
     # Initialize players
     players = new_players()
-
-    # Set game state
-    state = False
 
     # Return to game
     return board1, board2, players, state
 
 def new_players():
-    # Assign player names and characters
-    players = {}
-    players["Player1"] = [input("Player 1: "), 0]
+    # Initialize players dict
+    players = {
+        "Player1": {"Name": "", "Choice": "", "Turn": None, "Score": 0},
+        "Player2": {"Name": "", "Choice": "", "Turn": None, "Score": 0}
+    }
+
+    # print("DEBUG - Players init")
+    # print(players)
+
+    # Assign player1 name
+    players["Player1"]["Name"] = input("Name (Player1): ")
     
-    # Validate character
-    valid = ["X", "O"]
+    # Validate choice character
+    valid = ("X", "O")
     while True:
-        try:
-            # Assign player 1 choice
-            choice = input("Pick X or O: ").upper()
-        except ValueError:
-            print("Needs to be a letter")
+
+        # Assign player 1 choice
+        choice = input("Pick X or O: ").upper()
         if choice not in valid:
             print("Invalid choice")
         else:
-            players["Player1"][1] = choice
-            print(f"{players["Player1"][0]} is {choice}")
+            print(f"{players["Player1"]["Name"]} is {choice}")
+            players["Player1"]["Choice"] = choice
             break
     
-    # Assign player 2 choice
-    players["Player2"] = [input("Player 2: "), 0]
+    # Assign player 2 name and choice
+    players["Player2"]["Name"] = input("Player 2: ")
     if choice == "X":
-        players["Player2"][1] = "O"
+        players["Player2"]["Choice"] = "O"
     else:
-        players["Player2"][1] = "X"
-    print(f"{players["Player2"][0]} is {players["Player2"][1]}")
-    players["Player2"][1] = "O"
+        players["Player2"]["Choice"] = "X"
+    print(f"{players["Player2"]["Name"]} is {players["Player2"]["Choice"]}")
+
+    # print("DEBUG - Players choice")
+    # print(players)
+
+    # Determine first and second player
+    key = ['1', '2']
+    while True:
+        first = input("Who will start? (1/2): ")
+        if first in key:
+            
+            # Convert position to dict key
+            key.pop(int(first) - 1)
+            first = "Player" + first
+            second = "Player" + key[0]
+
+            # Assign positions
+            players[first]["Turn"] = True
+            players[second]["Turn"] = False
+
+            # print("DEBUG - Players positions")
+            # print(players)
+            break
+        else:
+            print("Type 1 or 2")
 
     # Return to start
     return players
-
 
 def score():
     pass
@@ -92,6 +129,7 @@ def new_boards():
 def print_board(board, selections, is_select):
     # Format board
     if not is_select:
+
         # Current game board
         print("Current: ", end='\n\n')
         for row1 in board[:1]:
@@ -101,6 +139,7 @@ def print_board(board, selections, is_select):
         for row3 in board[2:]:
             print("   ".join(row3), end='\n\n')
     else:     
+        
         # Available selection options
         print("Selection: ", end='\n\n')
         for i in range(3):
@@ -113,8 +152,8 @@ def print_board(board, selections, is_select):
 
 def print_players(players): # DEBUG
     # {Player#: [Name, Choice (X or O)]}
-    print(players["Player1"][0])
-    print(players["Player2"][0])
+    print(players["Player1"]["Name"])
+    print(players["Player2"]["Name"])
 
 def end():
     pass
@@ -124,3 +163,9 @@ def reset():
 
 # Start game
 game()
+
+
+# DEBUG
+    
+
+
