c832f2c928aabb6f9cededc3ed0990b4027fd0bc
agee <149341901+Agee-M@users.noreply.github.com>
2025-10-05 21:11:57 -0700

    Add play functionality and small refactors
    Completed game functionality:
    - Transferred bulk of game() into play() and helper function
    - Added board interactivity (choice prints to board)
    - Finalized gameplay loop (winner decided)
    - Refactored variables for easier set comparisons
    - Removed unused argument passes

--- game.py
+++ game.py
@@ -1,43 +1,103 @@
 # TODO: Tic-tac-toe game implementation
 
 def game():
+    # First game
+    playing = False
+
     # Initialize game
-    game_board, selection_board, players, is_select = start()
+    if not playing:
+        game_board, selection_board, players = start()
+    playing = True 
 
-    # Interactivity
-    if players["Player1"]["Turn"] == True:
-        input(f"{players["Player1"]["Name"]} select a spot: ")
-        players["Player1"]["Turn"] = False
-        players["Player1"]["Turn"] = True
+    # Game loop
+    while playing:
+        playing = play(game_board, selection_board, players)
 
-    else:
-        input(f"{players["Player2"]["Name"]} select a spot: ")
-        players["Player1"]["Turn"] = True
-        players["Player1"]["Turn"] = False
-
-
-    game_board[2][2] = 'X'
-    game_board[0][2] = 'O'
-    game_board[1][1] = 'X'
-    game_board[2][0] = 'X'
+        # Playing will return false when game is done
+        # TODO: Ask for playing again
     
-    # DEBUG
-    print_board(game_board, selection_board, is_select = False)
-    print()
-    print_board(game_board, selection_board, is_select = True)
-    #print_players(players)
 
     # Show positions as matrix (1 - 9) 
     # Combine game board with choice board (or just remove dots for choice board)
-    # TODO: Add interactivity between choices and board
-    # TODO: Add win conditions (including tie)
+    # Add interactivity between choices and board
+    # Add win conditions (including tie)
     # TODO: Add reset functionality
     # TODO: Add winner goes first?
     # TODO: Make 1 player or 2 player
 
-
+    print("EOF")
     return
 
+def play(game_board, selection_board, players): 
+    # Game state
+    playing = True
+    
+    # Helper variables
+    valid = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    match = {'1': (0, 0), '2':(0, 1), '3': (0, 2), '4': (1, 0), '5': (1, 1), '6': (1, 2), '7': (2, 0), '8': (2, 1), '9': (2, 2)}
+    win = [
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'},
+        {'1', '4', '7'},
+        {'2', '5', '8'},
+        {'3', '6', '9'},
+        {'1', '5', '9'},
+        {'3', '5', '7'}
+    ]
+    symbol_x = {}
+    symbol_y = {}
+    player = ""
+    
+    # Make choice
+    while True:
+
+        # Choose a spot
+        if players["Player1"]["Turn"] == True:
+            choice = input(f"{players["Player1"]["Name"]} select a spot: ")
+            player = "Player1"
+        else:
+            choice = input(f"{players["Player2"]["Name"]} select a spot: ")
+            player = "Player2"
+        
+        # Validate spot is open
+        if choice in valid:
+            break
+        else:
+            print("Choose an empty spot")
+
+    # Change player turns
+    swap(players)
+
+    # Track placements on board
+    print(choice)
+    players[player]["Placements"] += choice
+
+    # Add X or O inside of game_board
+    game_board[match[choice][0]][match[choice][1]] = players[player]["Symbol"]
+    print_board(game_board, selection_board, is_select = False)
+
+    # Remove option from available choices
+    for i, val in enumerate(valid):
+        if val == choice:
+            valid.pop(i)
+
+    # Check to see if 3 in a row
+    for i in win:
+        if set(players[player]["Placements"]) >= i:
+            print(f"{players[player]["Name"]} WINS!!!")
+            playing = False
+            return playing
+    return playing
+
+def swap(players):
+    # Change player turn
+    if players["Player1"]["Turn"] == True:
+        players["Player1"]["Turn"] = False
+        players["Player2"]["Turn"] = True
+    else:
+        players["Player1"]["Turn"] = True
+        players["Player2"]["Turn"] = False
 
 def start():
     print("New Game")
@@ -45,20 +105,17 @@ def start():
     # Initialize new boards
     board1, board2 = new_boards()
 
-    # Set selection state
-    state = False
-
     # Initialize players
     players = new_players()
 
     # Return to game
-    return board1, board2, players, state
+    return board1, board2, players
 
 def new_players():
     # Initialize players dict
     players = {
-        "Player1": {"Name": "", "Choice": "", "Turn": None, "Score": 0},
-        "Player2": {"Name": "", "Choice": "", "Turn": None, "Score": 0}
+        "Player1": {"Name": "", "Symbol": "", "Turn": None, "Score": 0, "Placements": ''},
+        "Player2": {"Name": "", "Symbol": "", "Turn": None, "Score": 0, "Placements": ''}
     }
 
     # print("DEBUG - Players init")
@@ -77,16 +134,16 @@ def new_players():
             print("Invalid choice")
         else:
             print(f"{players["Player1"]["Name"]} is {choice}")
-            players["Player1"]["Choice"] = choice
+            players["Player1"]["Symbol"] = choice
             break
     
     # Assign player 2 name and choice
     players["Player2"]["Name"] = input("Player 2: ")
     if choice == "X":
-        players["Player2"]["Choice"] = "O"
+        players["Player2"]["Symbol"] = "O"
     else:
-        players["Player2"]["Choice"] = "X"
-    print(f"{players["Player2"]["Name"]} is {players["Player2"]["Choice"]}")
+        players["Player2"]["Symbol"] = "X"
+    print(f"{players["Player2"]["Name"]} is {players["Player2"]["Symbol"]}")
 
     # print("DEBUG - Players choice")
     # print(players)
@@ -121,7 +178,7 @@ def score():
 def new_boards():
     # Creates 3 x 3 matrix
     board1 = [['.'] * 3 for _ in range(3)]
-    board2 = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+    board2 = [['9', '8', '7'], ['6', '5', '4'], ['3', '2', '1']]
     
     # Return to start
     return board1, board2
@@ -167,5 +224,9 @@ game()
 
 # DEBUG
     
+# BUG: Valid doesn't stop from pressing new inputs
+# BUG: Valid doesn't have functionality for min/complete removals
+# BUG: Input overwrites current position
+# TODO: Change names/intro
 
 
