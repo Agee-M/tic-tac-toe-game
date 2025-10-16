d68ac24b73a511f5f12832cd426bb8923e7e9221
agee <149341901+Agee-M@users.noreply.github.com>
2025-10-16 02:19:54 -0700

    Add full game functionality
    Fixed bugs in game logic:
    - Add input validation
    - Add debug class
    - Add init_board_map docs
    - Fix is_tie and is_win logic
    - Fix print_board logic
    - Fix random_move logic
    - Fix reset logic
    - Fix symbol assignment bug
    - Fix board setup logic
    - Fix play_turns logic

--- game_oop.py
+++ game_oop.py
@@ -1,13 +1,13 @@
 import random
 
-# Game, Board, Players, Debug
-
 class Board():
 
     # Initial board
     def __init__(self, board_size: int = 3):    
         if board_size < 3:
             board_size = 3
+        elif board_size > 10:
+            board_size = 10
         self.board = [["."] * board_size for i in range(board_size)]
 
     # Validate move on board
@@ -18,25 +18,16 @@ class Board():
             
     # Make move
     def make_move(self, choice: list[int, int], symbol: str):
-
-        # Check for invalid move     
-        if not self.is_valid(choice):
-            print("Invalid choice")
-            print()
-            return 
-        
-        # Update board
-        self.update_board(choice, symbol)
-
-    # Update board
-    def update_board(self, choice: list[int, int], symbol: str):
-        self.board[choice[0]][choice[1]] = symbol
+        self.board[choice[0]][choice[1]] = symbol   
         
     # Check tie
     def is_tie(self) -> bool:
-        if self.valid == []:
-            return True
-        return False
+        size = len(self.board)
+        for row in range(size):
+            for col in range(size):
+                if self.board[row][col] == '.':
+                    return False
+        return True
 
     # Check win
     def is_win(self, symbol: str) -> bool:
@@ -44,60 +35,60 @@ class Board():
         
         # Check rows
         win = True
+        r = c = 0
         for r in range(size):
             for c in range(size):
                 if self.board[r][c] == symbol:
+                    win = True
                     pass
                 else:
                     win = False
                     break
-
-        # Row win condition met
-        if win:
-            return True
+            # Row win condition met
+            if win:
+                return True
     
         # Check columns
         win = True
+        r = c = 0
         for c in range(size):
             for r in range(size):
                 if self.board[r][c] == symbol:
+                    win = True
                     pass
                 else:
                     win = False
                     break
-
-        # Column win condition met
-        if win:
-            return True
+            # Column win condition met
+            if win:
+                return True
     
         # Diagonal Algorithms   
         # Check \
         win = True
-        for r in range(size):
-            if self.board[r] == self.board[c] == symbol:
+        for i in range(size):
+            if self.board[i][i] == symbol:
                 pass
             else:
                 win = False
             if not win:
                 break
-
         # Diagonal 1 win condition met
         if win:
             return True
         
         # Check /
         win = True
-        row_ind = 0
-        col_ind = size - 1
+        r = 0
+        c = size - 1
         for _ in range(size):
-            if self.board[row_ind] == self.board[col_ind] == symbol:
-                row_ind += 1
-                col_ind -= 1
+            if self.board[r][c] == symbol:
+                r += 1
+                c -= 1
                 pass
             else:
                 win = False
-                break
-        
+                break       
         # Diagonal 2 win condition met
         if win:
             return True
@@ -106,7 +97,12 @@ class Board():
 
     # Print board
     def print_board(self):
-        print(self.board)
+        for i in range(len(self.board)):
+            for row in self.board[i]:
+                print("".join(row), end='   ')
+            print()
+            print()
+        print()
 
 class Player():
     
@@ -124,48 +120,53 @@ class Player():
 class Game():
 
     # Initialize a board, game, and 2 players
-
     def __init__(self):
-        self.game_board = Board()
+        self.game_board = None
         self.board_positions = {}
-        self.player1 = Player(1)
-        self.player2 = Player(2)
+        self.player1 = Player()
+        self.player2 = Player()
+        self.round = 0
+        self.board_size = 0
 
-        self.init_board_positions() 
 
     # Initialize board positions for game
-    def init_board_positions(self):
+    def init_board_map(self):
+        """game_board.board[0][total squares], but board positions maps it
+        in a human readable way, '1' = (0, 0). When interfacing with board.positions,
+        make sure to shift the iteration by 1. Ex; 3x3 board has size of 9 ->
+        for _ in range(1, 10)"""
+
         size = len(self.game_board.board)
         self.board_positions = {}
-        row_ind = 0
-        col_ind = 0
+        r = 0
+        c = 0
         for i in range(1, (size * size) + 1):   
-            matrix = [row_ind, col_ind]
+            matrix = [r, c]
             self.board_positions[str(i)] = matrix
             if i % size == 0:
-                row_ind += 1
-                col_ind = 0
+                r += 1
+                c = 0
             else:
-                col_ind += 1
+                c += 1
 
     # Generate computer move
     def random_move(self) -> int:
         while True:
-
-            # BUG: Possible issue with randrange and what is_valid() accepts
-            # Check if is_valid accepts 0-9 or 1-10
-            choice = str(random.randrange(1, len(self.board) * len(self.board) + 1))
+            choice = str(random.randrange(1, len(self.game_board.board) * len(self.game_board.board) + 1))
+            choice = self.choice_to_board(choice)
             if self.game_board.is_valid(choice):
                 return choice
             
 
     # Converts choice to board coordinates
-    def choice_to_board(self, choice: str) -> dict[int, int]:
+    def choice_to_board(self, choice: str) -> list[int, int]:
         return self.board_positions[choice]
     
     # Reset game
-    def reset(self):
-        self.board = Board()
+    def reset(self, board_size: int):
+        self.round += 1
+        self.game_board = Board(board_size)
+        self.run(self.round)
 
     # Swap players
     def swap_turns(self):
@@ -198,8 +199,13 @@ class Game():
         while True:
             ans = input(f"{self.player1.name} - X or O? (X/O): ").upper()
             print()
-            if ans == 'X' or ans == 'O':
+            if ans == 'X':
                 self.player1.symbol = ans
+                self.player2.symbol = 'O'
+                break
+            elif ans == 'O':
+                self.player1.symbol = ans
+                self.player2.symbol = 'X'
                 break
             else:
                 print("Choose X or O")
@@ -222,57 +228,149 @@ class Game():
     
     # Set up board
     def board_setup(self):
+        
+        # Choose board size
         while True:
             try:
-                size = input("Choose a board size: ")
+                self.board_size = int(input("Choose a board size: "))
             except ValueError:
                 print("Give a number")
             else:
                 break
+        
+        # Set up game board
+        self.game_board = Board(self.board_size)
+
+        # Set board map
+        self.init_board_map()
+
+    # Get player move
+    def get_move(self, name: str) -> str:
+        while True:
+            try:
+                choice = int(input(f"{name} pick an option (1-{len(self.game_board.board) * len(self.game_board.board)}): "))
+                print()
+            except ValueError:
+                print("Input number")
+                print()
+            else:
+                break
+        return str(choice)
+    
+    # Validate move input
+    def is_between(self, choice: str) -> bool:
+        return 0 < int(choice) <= len(self.game_board.board) * len(self.game_board.board)
 
     # Make a play
-    def play_turns(self, curr_player: object, next_player: object):
+    def play_turns(self, curr_player: object):
         
-        # BUG: make_move() accepts list[int, int], not str
-
-        # First player
-        if curr_player.is_human:
-            choice = input(f"{curr_player.name} pick an option (1-{len(self.board)}): ")
-        else:
+        # AI move
+        if not curr_player.is_human:
             choice = self.random_move()
-        self.board.make_move(choice, curr_player.symbol)
+            self.game_board.make_move(choice, curr_player.symbol)
+            return
+        
+        while True:
+            
+            # Get move
+            choice = self.get_move(curr_player.name)
 
-        # Print board
-        self.game_board.print_board()
+            # Validate input    
+            if not self.is_between(choice):
+                print("Invalid number")
+                print()
+                continue
 
-        # Second player
-        if next_player.is_human:
-            choice = input(f"{next_player.name} pick an option (1-{len(self.board)}): ")
-        else:
-            choice = self.random_move()
-        self.board.make_move(choice, next_player.symbol)
+            # Convert choice
+            choice = self.choice_to_board(str(choice))
+
+            # Validate board
+            if self.game_board.is_valid(choice):
+                break
+            else:
+                print("Pick an empty spot!")
+                print()
+                self.game_board.print_board()
+       
+        # Update board
+        self.game_board.make_move(choice, curr_player.symbol)
 
         # Print board
         self.game_board.print_board()
 
     # Run game
-    def run(self):
+    def run(self, round: int = 0):
+        
+        # Game setup
+        if round == 0:
+            self.player_setup()
+            self.board_setup()
 
         print("Game start!")
         print()
 
-        if self.player1.is_turn:
-            self.play_turns(self.player1, self.player2)
-        else:
-            self.play_turns(self.player2, self.player1)
-        
-        # Swap turns
-        self.swap_turns()
+        # Print default board
+        self.game_board.print_board()
 
-# TODO: Finish AI implementation
-# TODO: Fix bug with choice
-# TODO: Check whether passing obj works
-# TODO: Implement game loop
-# TODO: Implement reset
-# TODO: Test
-# TODO: Implement unit testing
+        # Game loop
+        while True:
+            current_player = ""
+            current_symbol = ""
+
+            # Make turn
+            if self.player1.is_turn:
+                self.play_turns(self.player1)
+                current_player = self.player1.name
+                current_symbol = self.player1.symbol
+            else:
+                self.play_turns(self.player2)
+                current_player = self.player2.name
+                current_symbol = self.player2.symbol
+
+            # Check for win
+            if self.game_board.is_win(current_symbol):
+                print(f"{current_player} won!")
+                print()
+                break
+
+            # Check for tie
+            if self.game_board.is_tie():
+                print("It's a tie!")
+                print()
+                break
+
+            # Swap turns
+            self.swap_turns()
+
+        # Prompt for reset
+        while True:
+            ans = input("Do you want to play again? (Y/N): ").upper()
+            if ans.startswith("Y"):
+                self.reset(self.board_size)
+            else:
+                break
+
+        # End
+        print("End game")
+        print()
+
+class Debug:
+    # Classes
+    def __init__(self):
+        self.game_d = Game()
+        self.board_d = Board()
+        self.player1_d = Player()
+        self.player2_d = Player()
+    
+    # Test run script
+    def play_test(self):
+        pass
+
+    # Win conditions
+    def win(self):
+        self.game_d.board_setup()
+        print(self.board_d.is_win('x'))
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
